4eb7f126515c4ad1ff46a2856b0e07e2a7e6e68d
Dante Ramos Montiel <danteramosmontiel67@gmail.com>
2026-08-19 14:41:55 -0300

    anashe

--- projects/app/src/App.jsx
+++ projects/app/src/App.jsx
@@ -5,6 +5,9 @@ export default function App() {
         <>
             <Card userName="danteramosss" name="Dante Ramos M." isFollowing={false} />
             <Card userName="midudev" name="Ángel Midudev" isFollowing={true} />
+            <Card userName="MatiiCont" name="Matias nigga" isFollowing={false} />
+            <Card userName="macasanmartinr" name="macuca" isFollowing={false} />
+            <Card userName="nicoo__aguirre" name="nico" isFollowing={true} />
         </>
     )
 }
