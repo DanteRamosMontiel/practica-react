b6038f4a5d4fab549b89f4ac0fa1bce1ef360bf8
Dante Ramos Montiel <danteramosmontiel67@gmail.com>
2026-08-21 21:59:08 -0300

    sdak{lfjkjashf

---
+++ projects/counter/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import background from "./assets/midnight-horizon.png"
+
+
+function App() {
+  return (
+    <>
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+    </>
+  )
+}
+
+export default App
